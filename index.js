32e017014d0f74b6d168bc106c6eea2d3b14e146
Ashutosh Dubey <ashutoshdubey2825@gmail.com>
2025-09-07 19:12:51 +0530

    prototype

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function reloadWebsite() {
   setInterval(reloadWebsite, interval);
 app.use(express.json());
 app.use(cors({
-  origin: "https://ecommerce-frontend-sand-ten.vercel.app/",
+  origin: "https://ecommerce-frontend-sand-ten.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
