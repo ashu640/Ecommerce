6bf32ae174701b165aac0029d3ac346aa43eec96
Ashutosh Dubey <ashutoshdubey2825@gmail.com>
2025-10-24 14:14:00 +0530

    Login problem solution

--- utils/trycatch.js
+++ utils/trycatch.js
@@ -1,14 +1,15 @@
-const TryCatch=(handler)=>{
-    return async(req,res,next)=>{
-        try {
-            await handler(req,res,next);
-        } catch (error) {
-            res.status(500).json({
-                message:error.message,
-            });
-            
-        }
-       
-    }
-};
-export default TryCatch;
+const TryCatch = (handler) => {
+    return async (req, res, next) => {
+      try {
+        await handler(req, res, next);
+      } catch (error) {
+        console.error("💥 Error:", error.message);
+        return res.status(500).json({
+          success: false,
+          message: error.message || "Internal Server Error",
+        });
+      }
+    };
+  };
+  export default TryCatch;
+  
